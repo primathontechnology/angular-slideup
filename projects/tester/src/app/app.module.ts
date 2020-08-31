70bdc64d558fc56ebae3b61099420d5787619c08
ram <ramvishvas.kumar@primathon.in>
2020-08-31 08:49:01 +0530

    [Ready To Deploye]

--- projects/tester/src/app/app.module.ts
+++ projects/tester/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 // import { AngularSlideUpModule } from 'projects/angular-slide-up/src/lib/angular-slide-up.module';
-import { AngularSlideUpModule } from "projects/angular-slide-up/src/public-api";
-// import { AngularSlideUpModule } from "angular-slide-up";
+// import { AngularSlideUpModule } from "projects/angular-slide-up/src/public-api";
+import { AngularSlideUpModule } from "angular-slide-up";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AngularSlideUpModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, AngularSlideUpModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
